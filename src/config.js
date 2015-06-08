ee797ce791533990ee57f1fdb08c8a32a8566c97
Alastair Pharo <asppsa@gmail.com>
2015-06-08 18:31:29 +1000

    Add sourceMap support
    Not really heavily tested, but seems to work in Firefox and Chrome.
    SourceMap is embedded in a comment in the JS file.  The comment is
    deleted during optimization.

--- src/config.js
+++ src/config.js
@@ -4,8 +4,12 @@ exports.defaults = function() {
   return {
     livescript: {
       extensions: ["ls"],
+      sourceMapDynamic: true,
+      sourceMapExclude: [], //[/\/specs?\//, /_spec.js$/],
+      sourceMapConditional: false,
       options: {
-        bare: true
+        bare: true,
+        sourceMap:true
       }
     }
   };
@@ -16,7 +20,11 @@ exports.placeholder = function() {
          "  livescript:              # config settings for the LiveScript compiler module\n" +
          "    lib: undefined         # use this property to provide a specific version of LiveScript\n" +
          "    extensions: [\"ls\"]     # default extensions for LiveScript files\n" +
+         "    sourceMapDynamic: true   # whether or not to inline the source maps in the compiled JavaScript\n" +
+         "    sourceMapExclude: [/\\/specs?\\//, /_spec.js$/] # files to exclude from source map generation\n" +
+         "    sourceMapConditional: false # whether or not to use conditional source maps\n" +
          "    options:               # options for the LiveScript compiler\n" +
+         "      sourceMap:true         # whether or not to create source maps\n" +
          "      bare:true            # whether or not to use the default safety wrapper\n";
 };
 
--- src/index.js
+++ src/index.js
@@ -8,10 +8,30 @@ var config = require( "./config" )
 var compile = function ( mimosaConfig, file, cb ) {
   var output,
     error,
-    lsConfig = mimosaConfig.livescript;
+    lsConfig = mimosaConfig.livescript,
+    options = Object.create( mimosaConfig.livescript.options );
+
+  // Check if source maps have been excluded for this file
+  if ( options.sourceMap ) {
+    if ( lsConfig.sourceMapExclude && lsConfig.sourceMapExclude.indexOf( file.inputFileName ) > -1 ) {
+      options.map = false;
+    } else if ( lsConfig.sourceMapExcludeRegex && file.inputFileName.match( lsConfig.sourceMapExcludeRegex ) ) {
+      options.map = false;
+    }
+    else {
+      options.filename = 'file://' + file.inputFileName;
+      options.map = 'embedded';
+    }
+  }
+  else {
+    options.map = false;
+  }
 
   try {
-    output = lsConfig.lib.compile( file.inputFileText, lsConfig.options );
+    output = lsConfig.lib.compile( file.inputFileText, options );
+    if (options.map) {
+      output = output.code;
+    }
   } catch ( err ) {
     error = err;
   }
